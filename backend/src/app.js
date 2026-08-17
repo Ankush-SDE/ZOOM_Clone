8fcec9d9df244d77488bdae7bd97f0be03de9034
Ankush Kumar <ankush1524kumar@gmail.com>
2026-08-17 19:49:37 +0530

    update code

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ import mongoose from "mongoose";
 
 import {connectToSocket} from "./controllers/socketManager.js";
 import cors from "cors";
-
+//import userRoutes from"./routes/users.routes.js";
 const app = express();
 const server = createServer(app);
 const io =connectToSocket(server);
